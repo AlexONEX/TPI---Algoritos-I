f1d7b93ff417882ea7cd12f635878527db7da3be
Schwartzmann <a.schwartzmann@hotmail.com>
2020-11-09 08:34:49 -0300

    Agregue test cases para el 12 y 10

--- tests/EJ12_menorSuperficieVivaTEST.cpp
+++ tests/EJ12_menorSuperficieVivaTEST.cpp
@@ -14,7 +14,7 @@ TEST(menorSuperficieVivaTEST, enunciado){
     EXPECT_EQ(res, 6);
 }
 
-TEST(menorSuperficieVivaTEST, 2por2){
+TEST(menorSuperficieVivaTEST, DosporDos){
     toroide t = {
             {true, false, false, true},   // 1
             {false, false, false, true}, // 2
@@ -34,3 +34,32 @@ TEST(menorSuperficieVivaTEST, noCuadrado){
     int res = menorSuperficieViva(t);
     EXPECT_EQ(res, 6);
 }
+
+TEST(menorSuperficieVivaTEST, CuatroPorCuatro){
+    toroide t ={
+            {false, true, true, true},   // 1
+            {false, false, false, false}, // 2
+            {false, false, false, false},
+            {false, true, true, false}};
+    int res = menorSuperficieViva(t);
+    EXPECT_EQ(res, 6);
+}
+TEST(menorSuperficieVivaTEST, CuatroPorCuatro2){
+    toroide t ={
+            {false, true, true, false},   // 1
+            {false, false, false, false}, // 2
+            {false, false, false, false},
+            {false, true, true, false}};
+    int res = menorSuperficieViva(t);
+    EXPECT_EQ(res, 4);
+}
+TEST(menorSuperficieVivaTEST, CuatroPorCuatro3){
+    toroide t ={
+            {true, true, false, false},   // 1
+            {false, false, false, false}, // 2
+            {false, false, false, true},
+            {false, true, true, false}};
+    int res = menorSuperficieViva(t);
+    EXPECT_EQ(res, 12);
+}
+
